885bf8fdc2930648d2fc17b54dc1656d4f35a31d
amirhanaknazar <amirhanaknazar1@gmail.com>
2026-01-30 19:11:49 +0500

    PP2

---
+++ practise1/pythonOutput.py
@@ -0,0 +1,11 @@
+print("Hello World!")
+print("I am learning Python.")
+print("It is awesome!")
+
+print("This will work!")
+print('This will also work!')
+
+print("Hello World!", end=" ")
+print("I will print on the same line.")
+
+print(This will cause an error)
