80e7ff61590be695a6bc49465c24297076080fbc
amirhanaknazar <amirhanaknazar1@gmail.com>
2026-01-30 19:13:46 +0500

    PP2

--- practise1/pythonOutput.py
+++ practise1/pythonOutput.py
@@ -9,3 +9,12 @@ print("Hello World!", end=" ")
 print("I will print on the same line.")
 
 print(This will cause an error)
+
+print(3)
+print(358)
+print(50000)
+
+print(3 + 3)
+print(2 * 5)
+
+print("I am", 35, "years old.")
