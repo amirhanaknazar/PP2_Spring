7881b516951b2a88b26f001e6090a8d6511b392e
amirhanaknazar <amirhanaknazar1@gmail.com>
2026-02-04 00:54:39 +0500

    PP2

---
+++ practise2/tuples/6.py
@@ -0,0 +1,8 @@
+tuple1 = ("a", "b" , "c")
+tuple2 = (1, 2, 3)
+
+tuple3 = tuple1 + tuple2
+print(tuple3)
+
+#count()	Returns the number of times a specified value occurs in a tuple
+#index()	Searches the tuple for a specified value and returns the position of where it was found
