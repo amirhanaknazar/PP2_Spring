ed2c5b9944097285bed24c5091d63dc30d709031
amirhanaknazar <amirhanaknazar1@gmail.com>
2026-02-13 21:15:04 +0500

    Complete Practice 3: Python functions, lambda, classes, and inheritance examples

---
+++ practise3/classes/class.py
@@ -0,0 +1,27 @@
+"""
+Python is an object oriented programming language.
+
+Almost everything in Python is an object, with its properties and methods.
+
+A Class is like an object constructor, or a "blueprint" for creating objects.
+
+"""
+#To create a class, use the keyword class:
+class MyClass:
+  x = 5
+
+#Create an object named p1, and print the value of x:
+p1 = MyClass()
+print(p1.x)
+
+#Delete the p1 object:
+del p1
+
+#Create three objects from the MyClass class:
+p1 = MyClass()
+p2 = MyClass()
+p3 = MyClass()
+
+print(p1.x)
+print(p2.x)
+print(p3.x)
