a005488fcaa633783b1b358ed0febf164e4bf738
amirhanaknazar <amirhanaknazar1@gmail.com>
2026-02-13 21:28:46 +0500

    Complete Practice 3: Python functions, lambda, classes, and inheritance examples

---
+++ practise3/inheritance/inheritance.py
@@ -0,0 +1,37 @@
+"""
+Inheritance allows us to define a class that inherits all the methods
+and properties from another class.
+
+Parent class is the class being inherited from, also called base class.
+
+Child class is the class that inherits from another class, also called derived class.
+
+"""
+
+#Create a class named Person, with firstname and lastname properties, and a printname method:
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+#Use the Person class to create an object, and then execute the printname method:
+
+x = Person("John", "Doe")
+x.printname()
+
+#Create a class named Student, which will inherit the properties and methods from the Person class:
+class Student(Person):
+  pass
+
+#Use the Student class to create an object, and then execute the printname method:
+x = Student("Mike", "Olsen")
+x.printname()
+
+#Add the __init__() function to the Student class:
+
+class Student(Person):
+  def __init__(self, fname, lname):
+    Person.__init__(self, fname, lname)
